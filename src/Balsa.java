7a085f82ecb67c31652b4ce5133a58a92d0c3dd3
FP <mariojd29@gmail.com>
2025-11-24 12:57:46 +0100

    Código Balsa
    Aquí he creado tres atributos con el respectivo
    constructor de Balsa y sus getter

--- src/Balsa.java
+++ src/Balsa.java
@@ -1,15 +1,32 @@
 public class Balsa {
+    private  String nombre;
+    private  int capacidad;
+    private  double tiempoSegundos; // tiempo en segundos (puede ser no entero)
 
-    private int capacidad;
-    private int tiempo;
-
-    public Balsa(int capacidad, int tiempo) {
+    public Balsa(String nombre, int capacidad, double tiempoSegundos) {
+        this.nombre = nombre;
         this.capacidad = capacidad;
-        this.tiempo = tiempo;
+        this.tiempoSegundos = tiempoSegundos;
+    }
+
+    public String getNombre() {
+        return nombre;
     }
 
     public int getCapacidad() {
         return capacidad;
     }
 
+    public double getTiempoSegundos() {
+        return tiempoSegundos;
+    }
+
+    @Override
+    public String toString() {
+        return "Balsa{" +
+                "nombre='" + nombre + '\'' +
+                ", capacidad=" + capacidad +
+                ", tiempoSegundos=" + tiempoSegundos +
+                '}';
+    }
 }
