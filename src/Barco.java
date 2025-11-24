0deefd0a38bd2ea2dacf61a20187910038f268c1
FP <mariojd29@gmail.com>
2025-11-24 13:03:15 +0100

    Codigo Barco
    Solo tiene el atributo de la lista de pasajeros, creo métodos synchronized para que por ejemplo si una balsa quiere recoger un pasajero y otra también va a recoger un pasajero, tiene que esperar a que la primera recoja al pasajero para que la otra balsa pueda recogerlo

--- src/Barco.java
+++ src/Barco.java
@@ -3,14 +3,42 @@ import java.util.List;
 
 public class Barco {
 
-    private final List<Pasajero> pasajeros = new ArrayList<>();
+    private  List<Pasajero> pasajeros = new ArrayList<>();
 
     public Barco(List<Pasajero> pasajeros) {
         this.pasajeros.addAll(pasajeros);
     }
 
-    public boolean hayPasajeros(){
+    public synchronized boolean hayPasajeros() {
         return !pasajeros.isEmpty();
     }
 
+
+    public synchronized List<Pasajero> obtenerPasajerosParaBalsa(int capacidad) {
+        List<Pasajero> recogidos = new ArrayList<>(capacidad);
+        if (pasajeros.isEmpty()) return recogidos;
+
+        // Recorremos prioridades 1 hasta 4
+        for (int prioridad = 1; prioridad <= 4 && recogidos.size() < capacidad; prioridad++) {
+
+            // Recorremos la lista por índice
+            for (int i = 0; i < pasajeros.size() && recogidos.size() < capacidad; i++) {
+
+                Pasajero p = pasajeros.get(i);
+
+                if (p.getPrioridad() == prioridad) {
+                    recogidos.add(p);
+                    pasajeros.remove(i);
+                    i--;
+                }
+            }
+        }
+
+        return recogidos;
+    }
+
+
+    public synchronized int pasajerosRestantes() {
+        return pasajeros.size();
+    }
 }
